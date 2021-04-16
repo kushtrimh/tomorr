bb8b3be63e04d8f4f0714b63644178bf0b1a4e8b
kushtrimh <kushtrimmh13@gmail.com>
2021-04-16 20:21:50 +0200

    Initial commit

---
+++ src/main/java/com/kushtrimh/tomorr/TomorrApplication.java
@@ -0,0 +1,13 @@
+package com.kushtrimh.tomorr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomorrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomorrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushtrimh/tomorr/TomorrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushtrimh.tomorr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
