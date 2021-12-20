4ee73e86448792d3736ea9d35606437d376c3156
kushtrimh <kushtrimmh13@gmail.com>
2021-12-20 22:49:49 +0100

    Fix bug on start-server script for appspec

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 cp /etc/tomorr/application.yml $TOMORR_S3_EXTERNAL_PROPS
+aws s3 cp $TOMORR_S3_EXTERNAL_PROPS /etc/tomorr/application.yml
 nohup /usr/java/jdk-17.0.1/bin/java -jar /usr/tmp/tomorr-*.jar --spring.config.additional-location=optional:/etc/tomorr/application.yml > /var/log/tomorr/tomorr.log 2>&1 &
 echo "Start command executed"
