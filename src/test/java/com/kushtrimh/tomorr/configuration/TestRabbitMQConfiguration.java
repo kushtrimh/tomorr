9ed0e169a465338012bdb8fc10e38826bdf53a70
kushtrimh <kushtrimmh13@gmail.com>
2022-05-02 18:14:57 +0200

    Add integration testing for RabbitMQ listener

--- src/test/java/com/kushtrimh/tomorr/configuration/TestRabbitMQConfiguration.java
+++ src/test/java/com/kushtrimh/tomorr/configuration/TestRabbitMQConfiguration.java
@@ -2,6 +2,12 @@ package com.kushtrimh.tomorr.configuration;
 
 import com.kushtrimh.tomorr.extension.TestRabbitMQExtension;
 import com.kushtrimh.tomorr.properties.RabbitMQProperties;
+import com.kushtrimh.tomorr.task.Task;
+import com.kushtrimh.tomorr.task.data.ArtistTaskData;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.test.TestRabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,5 +27,19 @@ public class TestRabbitMQConfiguration {
         properties.setUsername("guest");
         properties.setPassword("guest");
         return properties;
+    }
+
+    @Bean
+    public TestRabbitTemplate artistTaskRabbitTemplate(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter rabbitJsonMessageConverter) {
+        TestRabbitTemplate rabbitTemplate = new TestRabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(rabbitJsonMessageConverter);
+        return rabbitTemplate;
+    }
+
+    @RabbitListener(queues = "artistSync")
+    public String artistSyncListener(Task<ArtistTaskData> artistTask) {
+        return artistTask.getData().getArtistId();
     }
 }
