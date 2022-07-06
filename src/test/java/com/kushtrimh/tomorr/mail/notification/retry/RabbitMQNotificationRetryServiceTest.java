ad992541c3d3e6ed1827955477bc67b732ddf713
Kushtrim Hajrizi <kushtrimmh13@gmail.com>
2022-07-06 20:58:08 +0200

    Remove unneeded test

--- src/test/java/com/kushtrimh/tomorr/mail/notification/retry/RabbitMQNotificationRetryServiceTest.java
+++ src/test/java/com/kushtrimh/tomorr/mail/notification/retry/RabbitMQNotificationRetryServiceTest.java
@@ -1,11 +1,9 @@
 package com.kushtrimh.tomorr.mail.notification.retry;
 
-import com.kushtrimh.tomorr.configuration.RabbitMQConfiguration;
 import com.kushtrimh.tomorr.task.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.amqp.core.Message;
@@ -15,9 +13,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -40,16 +36,6 @@ class RabbitMQNotificationRetryServiceTest {
     public void retryNotification_WhenNotificationRetryDataIsNull_DoNothing() {
         service.retryNotification(null);
         verify(rabbitTemplate, never()).convertAndSend(anyString(), any(Task.class));
-    }
-
-    @Test
-    public void retryNotification_WhenNotificationRetryDataIsValid_SendNotificationDataToQueue() {
-        var notificationRetryData = new NotificationRetryData(
-                "from", "subject", "templateName", new HashMap<>(), new ArrayList<>());
-        service.retryNotification(notificationRetryData);
-
-        verify(rabbitTemplate, times(1)).convertAndSend(
-                eq(RabbitMQConfiguration.NOTIFICATION_RETRY_QUEUE), eq(new Task<>(notificationRetryData)), any(MessagePostProcessor.class));
     }
 
     @Test
